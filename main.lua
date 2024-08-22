0944d784e7bfe04c4dbbda45a2014c9dafd7c32d
Eva Pace <eba.pachi@gmail.com>
2024-08-22 14:55:21 -0300

    background scroll

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ love.graphics.setDefaultFilter('nearest', 'nearest')
 WIDTH, HEIGHT = 480, 270
 
 local player = { x = 6, y = HEIGHT / 3, speed = 200 }
-local background = { x = 0, y = 0, sprite = love.graphics.newImage('T_field.png') }
+local background = { x = 0, y = 0, sprite = love.graphics.newImage('T_field2.png'), speed = 50 }
 
 function love.load()
     love.window.setTitle('suit ninja')
@@ -46,12 +46,15 @@ function love.update(dt)
         player.x = player.x + player.speed * dt
     end
     player.currAnim:update(dt)
+
+    background.x = background.x + background.speed * dt
+    background.x = background.x % WIDTH
 end
 
 function love.draw()
     push:start()
 
-    love.graphics.draw(background.sprite, background.x, background.y)
+    love.graphics.draw(background.sprite, -background.x, background.y)
 
     local scale = 2
     player.currAnim:draw(player.spritesheet, player.x, player.y, 0, scale, scale)
