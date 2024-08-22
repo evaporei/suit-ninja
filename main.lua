246e89cd6ed2948b8777aa4e622ab833bb3537d4
Eva Pace <eba.pachi@gmail.com>
2024-08-22 15:20:41 -0300

    card coming at you

--- main.lua
+++ main.lua
@@ -6,9 +6,16 @@ love.graphics.setDefaultFilter('nearest', 'nearest')
 
 WIDTH, HEIGHT = 480, 270
 
+local lanes = {
+    [1] = HEIGHT / 3 + 8,
+    [2] = HEIGHT / 3 * 2 - 2,
+}
+
 local player = { x = 6, y = HEIGHT / 3 + 8, speed = 200 }
 local background = { x = 0, y = 0, sprite = love.graphics.newImage('T_field2.png'), speed = 50 }
-local lane = 1
+local currLane = 1
+local card = { y = lanes[1] + 20, sprite = love.graphics.newImage('cards/Hearts/Hearts_card_01.png') }
+card.x = WIDTH - card.sprite:getWidth() * 2
 
 function love.load()
     love.window.setTitle('suit ninja')
@@ -30,10 +37,10 @@ end
 
 function love.keypressed(key)
     if key == 'up' then
-        lane = 1
+        currLane = 1
     end
     if key == 'down' then
-        lane = 2
+        currLane = 2
     end
     if key == 'escape' then
         love.event.quit()
@@ -47,19 +54,15 @@ function love.update(dt)
     if love.keyboard.isDown('right') then
         player.x = player.x + player.speed * dt
     end
-    if lane == 1 then
-        Timer.tween(0.1, {
-            [player] = { y = HEIGHT / 3 + 8 }
-        })
-    elseif lane == 2 then
-        Timer.tween(0.1, {
-            [player] = { y = HEIGHT / 3 * 2 - 2 }
-        })
-    end
+    Timer.tween(0.1, {
+        [player] = { y = lanes[currLane] }
+    })
     player.currAnim:update(dt)
 
     background.x = background.x + background.speed * dt
     background.x = background.x % WIDTH
+
+    card.x = card.x - background.speed * dt
 
     Timer.update(dt)
 end
@@ -68,6 +71,8 @@ function love.draw()
     push:start()
 
     love.graphics.draw(background.sprite, -background.x, background.y)
+
+    love.graphics.draw(card.sprite, card.x, card.y, 0, 0.8, 0.8)
 
     local scale = 2
     player.currAnim:draw(player.spritesheet, player.x, player.y, 0, scale, scale)
